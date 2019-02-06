508634f8c5edb6d14ca69c625cccc8079cb2b55d
Wes Spiller <ws16063@bristol.ac.uk>
2019-02-06 15:54:07 +0000

    Initial_setup

---
+++ R/Challenge_dat.R
@@ -0,0 +1,24 @@
+#' Genetic, metabolite, and outcome data for the 2019 MR Conference Data Challenge.
+#'
+#' A dataset containing summary data for the estimated associations between 118 metabolite risk factors and
+#' 150 genetic variants, with metabolite information quantified using nuclear magnetic resonance (NMR) spectroscopy 
+#' metabolomics. The data also includes information on 7 outcomes; age-related macular degeneration, alzheimers's disease,
+#'  type 2 diabetes, Ischemic stroke, large artery stroke, cardioembolic stroke, and small vessel stroke.
+#' 
+#' Challenge_dat.
+#'
+#' @format A data frame with 150 rows and 267 variables. A full description of the data is available
+#' by using the command \code{vignette("MRDataChallenge19")}.
+#' 
+#' @source 
+#' \itemize{
+#' \item \url{http://www.computationalmedicine.fi/data#NMR_GWAS}
+#' \item \url{http://lipidgenetics.org/}
+#' \item \url{http://amdgenetics.org/}
+#' \item \url{http://diagram-consortium.org/downloads.html}
+#' \item \url{http://www.megastroke.org/}
+#' }
+
+#'@author Wes Spiller; Jack Bowden; Verena Zuber.
+
+"Challenge_dat"
